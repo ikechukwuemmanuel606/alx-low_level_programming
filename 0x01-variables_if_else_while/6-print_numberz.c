c42eff45003d859eae25f061fdfcc1c5e0dcddc9
ikechukwuemmanuel606 <ikechukwuemmanuel606@gmail.com>
2023-09-15 11:37:10 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - A program that prints numbers
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 58; a++)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+	return (0);
+}
