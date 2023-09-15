3ffd3ae38cb104a3aaf7949d43e00170fdebe51b
ikechukwuemmanuel606 <ikechukwuemmanuel606@gmail.com>
2023-09-15 11:14:13 -0700

    printing single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - A program that prints alphabets in lowercase
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+		printf("\n");
+	return (0);
+}
